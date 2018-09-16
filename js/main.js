e814efece2ab5c671d661ac8210d291f5909e78a
Tracy Fussell <tracy.fussell@gmail.com>
2018-09-16 09:14:00 -0400

    Move sw.js file to correct folder

--- js/main.js
+++ js/main.js
@@ -209,8 +209,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 if ('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js').then(function(registration))
-  .catch(function(err){
-    console.error(err);
-  });
+  navigator.serviceWorker.register('/sw.js').then(function(registration){
+    console.log('Service Worker registration succeeded:', registration);
+  })
+  (function(err){
+    console.log('Service Worker registration failed:', err);
+} else {
+  console.log('Service workers are not supported.');
 }
+);
